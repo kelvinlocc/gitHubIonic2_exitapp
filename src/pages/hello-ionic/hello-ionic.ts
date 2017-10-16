10a6c0936e5c08706c716562938512bd28deb4b6
kelvinlocc <ccloadust@gmail.com>
2017-10-17 00:25:09 +0800

    1

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,70 @@
 import { Component } from '@angular/core';
+import {Platform} from "ionic-angular";
+import { Http } from '@angular/http';
+
+interface Navigator {
+  app: {
+    exitApp: () => any; // Or whatever is the type of the exitApp function
+
+
+
+
+
+
+
+
+  }
+
+
+}
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  platform: any;
 
+ constructor(platform:Platform) {
+   this.platform = Platform;
   }
+
+
+  exitApp(){
+    // this.platform.exitApp();
+    // navigator.app.exitApp();
+    navigator['app'].exitApp();
+  }
+
+  displayMsn (){
+   console.log("my log ... ");
+    // this.platform.exitApp();
+    // app.this.platform.exitApp();
+    //
+    //
+    // navigator.app.exitApp();
+
+
+    // Platform.registerBackButtonAction(() => {
+    //   const overlay = this.app._appRoot._overlayPortal.getActive();
+    //   const nav = this.app.getActiveNav();
+    //
+    //   if(overlay && overlay.dismiss) {
+    //     overlay.dismiss();
+    //   } else if(nav.canGoBack()){
+    //     nav.pop();
+    //   } else if(Date.now() - this.lastBack > 500) {
+    //     this.platform.exitApp();
+    //   }
+    //   this.lastBack = Date.now();
+    // });
+    // platform.ready().then(() => {
+      // platform.registerBackButtonAction(() => {
+      //   platform.exitApp();
+      // });
+    // });
+  }
+
+
+
 }
